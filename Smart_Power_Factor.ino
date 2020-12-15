5107e869b0976a3e23b8ff0546fbef1dcb42937d
Faris Bagas <50089515+DinanCitizens@users.noreply.github.com>
2020-12-15 16:14:02 +0700

    Tambah Wifi ESP

--- Smart_Power_Factor.ino
+++ Smart_Power_Factor.ino
@@ -1,8 +1,30 @@
+#include <WiFiEsp.h>
 #include <PZEM004Tv30.h>
+
+char ssid[] = "DAPR";
+char pass[] = "dinan_99";
+
+int status = WL_IDLE_STATUS;
 PZEM004Tv30 pzem(11, 12);
+
 void setup() {
-Serial.begin(115200);
+Serial.begin(9600);
+Serial1.begin(115200);
+WiFi.init(&Serial1);
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi Module Tidak Ada");
+    while (true);
 }
+while ( status != WL_CONNECTED) {
+    Serial.print("Mencoba Terhubung ke WiFi dengan SSID: ");
+    Serial.println(ssid);
+    status = WiFi.begin(ssid, pass);
+  }
+  Serial.print("Kamu Telah Terhubung Ke WiFi: ");
+  Serial.println(ssid);
+  printWifiStatus();
+}
+
 void loop() {
 float voltage = pzem.voltage();
 if(voltage != NAN){
@@ -42,4 +64,16 @@ Serial.println("Error reading power factor");
 }
 Serial.println();
 delay(2000);
+}
+
+void printWifiStatus() {
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+  IPAddress ip = WiFi.localIP();
+  Serial.print("Alamat IP: ");
+  Serial.println(ip);
+  long rssi = WiFi.RSSI();
+  Serial.print("Kekuatan Sinyal (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
 }
