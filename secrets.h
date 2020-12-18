91c42f1833d924b137c14b7016b1210e53bfcd1f
Faris Bagas <50089515+DinanCitizens@users.noreply.github.com>
2020-12-18 14:42:24 +0700

    Konfigurasi WiFi

---
+++ secrets.h
@@ -0,0 +1,5 @@
+#define SECRET_SSID "DAPR"    // Nama WiFi
+#define SECRET_PASS "dinan_99"  // Password WiFi
+
+#define SECRET_CH_ID 1163360     // Nama Channel Thingspeak
+#define SECRET_WRITE_APIKEY "1CRTQ7N4FZ6D1Q8O"   // Write API Key Thingspeak
